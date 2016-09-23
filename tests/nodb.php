699d30ebabf20843e19e71296605d3c5f7969594
Nima <ghotbi.nima@gmail.com>
2016-09-23 12:20:46 +0300

    remove extra files

--- tests/nodb.php
+++
@@ -1,8 +0,0 @@
-<?php
-$servername = "localhost:5005";
-$username = "root";
-$password = "xX123456";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, "test");
-$conn.query("show tables");
--- tests/pgsql.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$link = pg_Connect("host=127.0.0.1 port=5432 dbname=postgres user=postgres password=xX123456") or die("failed");
-
-$result = pg_exec($link, "select * from playground;");
-echo pg_numrows($result);
-
