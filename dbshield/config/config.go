40cfbe7a0d6abf41237fda647b9b9d981aed5468
Nima <ghotbi.nima@gmail.com>
2016-09-25 14:34:51 +0300

    Added default values for configs

--- dbshield/config/config.go
+++ dbshield/config/config.go
@@ -49,74 +49,140 @@ var Config = struct {
 	Masks map[string]mask
 }{}
 
+func strConfig(dst *string, key, defaultValue string) {
+	if viper.IsSet(k) {
+		dst = viper.GetString(k)
+	} else {
+		logger.Infof("'%s' not configured, assuming: %s", key, defaultValue)
+		dst = defaultValue
+	}
+}
+
+func intConfig(dst *int, key string, defaultValue, min int) error {
+	if viper.IsSet(k) {
+		dst = uint(viper.GetInt(k))
+		if dst < min {
+			return fmt.Errorf("Invalid '%s' cofiguration: %v\n", k, dst)
+		}
+	} else {
+		logger.Infof("'%s' not configured, assuming: %s", key, defaultValue)
+		dst = defaultValue
+	}
+}
+
 //ParseConfig and return error if its not valid
 func ParseConfig(configFile string) error {
 	viper.SetConfigFile(configFile)
 	err := viper.ReadInConfig() // Find and read the config file
 	if err != nil {             // Handle errors reading the config file
-		return fmt.Errorf("Fatal error config file: %s \n", err)
-	}
-	Config.DBDir = viper.GetString("dbDir")
-	os.Mkdir(Config.DBDir, 0740)
-	//set default values
-	Config.DBType = viper.GetString("dbms")
-	switch viper.GetString("mode") {
-	case "protect":
-		Config.Learning = false
-	case "learning":
-		Config.Learning = true
-	default:
-		return errors.New("Invalid cofiguration: mode")
+		return fmt.Errorf("Fatal error - config file: %s \n", err)
 	}
 
-	for _, check := range strings.Split(viper.GetString("additionalChecks"), ",") {
-		switch check {
-		case "user":
-			Config.CheckUser = true
-		case "source":
-			Config.CheckSource = true
+	//set default values
+	if viper.IsSet("mode") {
+		switch viper.GetString("mode") {
+		case "protect":
+			Config.Learning = false
+		case "learning":
+			Config.Learning = true
 		default:
-			return errors.New("Invalid cofiguration: additionalChecks")
+			return errors.New("Invalid 'mode' cofiguration:", viper.GetString("mode"))
+		}
+	} else {
+		logger.Infof("'mode' not configured, assuming: learning")
+		Config.Learning = true
+	}
+
+	if viper.IsSet("additionalChecks") {
+		for _, check := range strings.Split(viper.GetString("additionalChecks"), ",") {
+			switch check {
+			case "user":
+				Config.CheckUser = true
+			case "source":
+				Config.CheckSource = true
+			default:
+				return errors.New("Invalid 'additionalChecks' cofiguration:", check)
+			}
 		}
 	}
 
-	switch viper.GetString("action") {
-	case "drop": //Close the connection
-		Config.Action = utils.ActionDrop
-	case "pass": //Pass the query to server
-		Config.Action = nil
-	default:
-		return errors.New("Invalid cofiguration: action")
+	if !Config.Learning {
+		if viper.IsSet("action") {
+			switch viper.GetString("action") {
+			case "drop": //Close the connection
+				Config.Action = utils.ActionDrop
+			case "pass": //Pass the query to server
+				Config.Action = nil
+			default:
+				return errors.New("Invalid 'action' cofiguration:", viper.GetString("action"))
+			}
+		} else {
+			logger.Infof("'action' not configured, assuming: drop")
+			Config.Action = utils.ActionDrop
+		}
 	}
 
-	Config.Threads = uint(viper.GetInt("threads"))
-	if Config.Threads < 1 {
-		return errors.New("Invalid cofiguration: threads")
+	if viper.IsSet("targetIP") {
+		Config.TargetIP = viper.GetString("targetIP")
+	} else {
+		return errors.New("Invalid 'targetIP' cofiguration:", viper.GetString("targetIP"))
 	}
-	Config.LogLevel = uint(viper.GetInt("logLevel"))
-	logger.Level = Config.LogLevel
 
-	Config.LogPath = viper.GetString("logPath")
+	if viper.IsSet("tlsPrivateKey") {
+		Config.TLSPrivateKey = viper.GetString("tlsPrivateKey")
+	} else {
+		return errors.New("Invalid 'tlsPrivateKey' cofiguration:", viper.GetString("tlsPrivateKey"))
+	}
 
-	Config.ListenIP = viper.GetString("listenIP")
-	Config.TargetIP = viper.GetString("targetIP")
+	if viper.IsSet("tlsCertificate") {
+		Config.TargetIP = viper.GetString("tlsCertificate")
+	} else {
+		return errors.New("Invalid 'tlsCertificate' cofiguration:", viper.GetString("tlsCertificate"))
+	}
 
-	Config.ListenPort = uint(viper.GetInt("listenPort"))
-	Config.TargetPort = uint(viper.GetInt("targetPort"))
+	//String values
+	strConfig(&Config.DBDir, "dbDir", os.TempDir()+"/model")
+	os.Mkdir(Config.DBDir, 0740) //Make dbDir, just in case its not there
 
-	Config.TLSPrivateKey = viper.GetString("tlsPrivateKey")
-	Config.TLSCertificate = viper.GetString("tlsCertificate")
+	strConfig(&Config.DBType, "dbms", "mysql")
+
+	strConfig(&Config.ListenIP, "listenIP", "0.0.0.0")
+
+	strConfig(&Config.LogPath, "logPath", "stderr")
+
+	//Integer values
+	err = intConfig(&Config.Threads, "threads", 4, 1)
+	if err != nil {
+		return err
+	}
+
+	err = intConfig(&Config.LogLevel, "logLevel", 2, 0)
+	if err != nil {
+		return err
+	}
+
+	err = intConfig(&Config.ListenPort, "listenPort", 0, 1)
+	if err != nil {
+		return err
+	}
+
+	err = intConfig(&Config.TargetPort, "targetPort", 0, 1)
+	if err != nil {
+		return err
+	}
 
 	Config.Masks = make(map[string]mask)
-	tmpMasks := viper.Get("masks").([]interface{})
-	for _, tm := range tmpMasks {
-		m := tm.(map[interface{}]interface{})
-		key := m["database"].(string) + "." + m["table"].(string) + "." + m["column"].(string)
-		padding := []byte(m["paddingCharacter"].(string))
-		Config.Masks[key] = mask{
-			MatchExp:         regexp.MustCompile(m["matchRegEx"].(string)),
-			ReplaceExp:       []byte(m["replaceRegEx"].(string)),
-			PaddingCharacter: []byte{padding[0]},
+	if viper.IsSet("masks") {
+		tmpMasks := viper.Get("masks").([]interface{})
+		for _, tm := range tmpMasks {
+			m := tm.(map[interface{}]interface{})
+			key := m["database"].(string) + "." + m["table"].(string) + "." + m["column"].(string)
+			padding := []byte(m["paddingCharacter"].(string))
+			Config.Masks[key] = mask{
+				MatchExp:         regexp.MustCompile(m["matchRegEx"].(string)),
+				ReplaceExp:       []byte(m["replaceRegEx"].(string)),
+				PaddingCharacter: []byte{padding[0]},
+			}
 		}
 	}
 	return nil
