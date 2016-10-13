706be98d93b04762be757065047d59ab60fc14d0
Nima <ghotbi.nima@gmail.com>
2016-10-13 23:03:08 +0300

    comments added

--- main.go
+++ main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"flag"
 	"log"
+	"runtime"
 
 	"github.com/nim4/DBShield/dbshield"
 )
 
 func main() {
+	runtime.GOMAXPROCS(runtime.NumCPU()) // For Go < 1.5
 	//Parsing command line arguments
 	config := flag.String("f", "/etc/dbshield.yml", "Config file")
 	flag.Parse()
