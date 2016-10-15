1d407a108115fc1e36eec1c7be891f9e5c36d8cd
Nima <ghotbi.nima@gmail.com>
2016-10-15 22:21:43 +0300

    tests added

---
+++ dbshield/utils_test.go
@@ -0,0 +1,16 @@
+package dbshield
+
+import "testing"
+
+func TestpascalString(t *testing.T) {
+	_, err := dbNameToStruct("mysql")
+	if err != nil {
+		t.Error("Expected struct, got ", err)
+		return
+	}
+	_, err = dbNameToStruct("invalid")
+	if err == nil {
+		t.Error("Expected error")
+		return
+	}
+}
